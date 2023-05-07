53de22cb11d590246ed56acae563623f01283d1f
zjh <1525090646@qq.com>
2023-05-07 16:22:26 +0800

    在kayson-spring-boot-starter-web，实现Swagger的基础封装

---
+++ kayson-framework/kayson-spring-boot-starter-web/src/main/java/cn/zjh/kayson/framework/swagger/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 基于 Swagger 实现 API 文档
+ * 
+ * @author zjh - kayson
+ */
+package cn.zjh.kayson.framework.swagger;
--- kayson-server/src/main/java/cn/zjh/kayson/server/controller/admin/TestController.java
+++ kayson-server/src/main/java/cn/zjh/kayson/server/controller/admin/TestController.java
@@ -1,6 +1,8 @@
 package cn.zjh.kayson.server.controller.admin;
 
 import cn.zjh.kayson.framework.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author zjh - kayson
  */
+@Tag(name = "Test 接口")
 @RestController
 @RequestMapping("/test")
 public class TestController {
     
+    @Operation(summary = "测试get方法")
     @GetMapping("/get")
     public CommonResult<String> get() {
         return CommonResult.success("test get success");
     }
     
+    @Operation(summary = "测试exception方法")
     @GetMapping("/exception")
     public CommonResult<String> exception() {
         throw new RuntimeException("test exception");
