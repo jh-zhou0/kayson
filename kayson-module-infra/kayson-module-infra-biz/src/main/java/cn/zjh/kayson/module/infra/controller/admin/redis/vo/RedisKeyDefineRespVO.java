5d39aa3a02b7424057952bff86305ea96fb2a37c
zjh <1525090646@qq.com>
2023-06-02 10:11:06 +0800

    完成 管理后台 - Redis 监控

---
+++ kayson-module-infra/kayson-module-infra-biz/src/main/java/cn/zjh/kayson/module/infra/controller/admin/redis/vo/RedisKeyDefineRespVO.java
@@ -0,0 +1,36 @@
+package cn.zjh.kayson.module.infra.controller.admin.redis.vo;
+
+import cn.zjh.kayson.framework.redis.core.RedisKeyDefine;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Duration;
+
+/**
+ * @author zjh - kayson
+ */
+@Schema(description = "管理后台 - Redis Key 信息 Response VO")
+@Data
+@Builder
+public class RedisKeyDefineRespVO {
+
+    @Schema(description = "Key 模板", requiredMode = Schema.RequiredMode.REQUIRED, example = "login_user:%s")
+    private String keyTemplate;
+
+    @Schema(description = "Key 类型的枚举", requiredMode = Schema.RequiredMode.REQUIRED, example = "String")
+    private RedisKeyDefine.KeyTypeEnum keyType;
+
+    @Schema(description = "Value 类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "java.lang.String")
+    private Class<?> valueType;
+
+    @Schema(description = "超时类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+    private RedisKeyDefine.TimeoutTypeEnum timeoutType;
+
+    @Schema(description = "过期时间，单位：毫秒", requiredMode = Schema.RequiredMode.REQUIRED, example = "1024")
+    private Duration timeout;
+
+    @Schema(description = "备注", requiredMode = Schema.RequiredMode.REQUIRED, example = "啦啦啦啦~")
+    private String memo;
+
+}
